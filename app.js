cf1d8d480d5d43411c9ddefb9785c282e7477418
Arkar Min <arkarmin@Arkars-MacBook-Air.local>
2023-01-15 23:07:56 +0630

    api with params

--- app.js
+++ app.js
@@ -26,6 +26,25 @@ app.get('/api/v1/tours',(req,res)=>{
     
 });
 
+app.get('/api/v1/tours/:id',(req,res)=>{
+    const id = req.params.id*1;
+    const tour = tours.find(el=>el.id===id);
+    if(!tour){
+        res.status(404).json({
+            'status':'fail',
+            'message':'Invalid ID'
+        });
+    }
+    res.status(200).json({
+        status:'success',
+        results:tour.length,
+        data:{
+            tour 
+        }
+    });
+    
+});
+
 app.post('/api/v1/tours',(req,res)=>{
     console.log(req.body);
     const newID = tours[tours.length-1].id+1;
